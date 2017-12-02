1bfe8fa6dd73d6d37ff45e0e43de072120137086
NKClem <nkuhn09@gmail.com>
2017-12-02 18:41:33 -0500

    started javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//array for buttons
+var topics = ['happy', 'sad', 'angry', 'joyful', 'annoyed', 'frustrated', 'excited', 'sleepy', 'crazy', 'depressed', 'silly', 'bored', 'brave', 'proud', 'foolish', 'lame', 'cheerful'];
+
+renderButtons();
+
+function renderButtons() {
+	$('#buttons').empty();
+	topics.forEach(function(feeling) {
+		var newButton = $('<button>');
+		newButton.addClass('style-buttons');
+		newButton.attr('data-name', feeling);
+		newButton.text(feeling);
+		$('#buttons').append(newButton);
+	})
+}
+
+$('#add-feeling').click(function() {
+	event.preventDefault();
+	var newFeeling = $('#new-feeling').val().trim();
+	topics.push(newFeeling);
+	console.log(topics);
+	renderButtons();
+})
